63acd833d872b55b62c43fa7c184441f0b667901
lindyem <lindy.merwin@gmail.com>
2020-09-01 15:01:54 -0700

    Uploading Images 236

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { db, auth } from "./firebase";
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import { Button, Input } from "@material-ui/core";
+import ImageUpload from './components/ImageUpload/ImageUpload';
 
 function getModalStyle() {
   const top = 50;
@@ -88,6 +89,14 @@ function App() {
 
   return (
     <div className="app">
+      {user.displayName ? (
+        <ImageUpload username={user.displayName} />
+      ) : (
+          <h3>Sorry you need to login.</h3>
+        )}
+     
+
+
       <Modal open={open} onClose={() => setOpen(false)}>
         <div style={modalStyle} className={classes.paper}>
           <form className="app__signup">
---
+++ src/components/ImageUpload/ImageUpload.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import { Button } from "@material-ui/core";
+import { storage, db } from "./firebase";
+
+function ImageUpload({username}) {
+  const [caption, setCaption] = useState('');
+  const [image, setImage] = useState(null);
+  const [url, setUrl] = useState("");
+  const [progress, setProgress] = useState(0);
+
+  const handleChange = (e) => {
+    if (e.targe.files[0]) {
+      setImage(e.targe.files[0]);
+    }
+  };
+
+  const handleUpload = () => {
+    const uploadTask = storage.ref(`images/${image.name}`).put(image);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(progress);
+      },
+      (error) => {
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        storage
+          .ref("image")
+          .child(image.name)
+          .getDownloadURL()
+          .then(url => {
+            db.collections("posts").add({
+              timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+              caption: caption,
+              imageUrl: url,
+              username: username
+          })
+        })
+      }
+    )
+  }
+
+
+  return(
+    <div>
+      <h1>abc</h1>
+      {/* I want to have... */}
+      {/* caption input */}
+      {/* file picker */}
+      {/* post button */}
+      <input type="text" placeholder="enter a caption" onChange={event=> setCaption(event.target.value)} value={caption}/>
+      <input type="file" onChange={handleChange} />
+      <Button className="imageupload__button" onClick={handleUpload}>Upload</Button> 
+    </div>
+  );
+}
+
+export default ImageUpload
