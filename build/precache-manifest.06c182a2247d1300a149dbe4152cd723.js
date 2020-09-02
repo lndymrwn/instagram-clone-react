77ebabfc7675d6aadc310196c2bf69984d0aca0a
lindyem <lindy.merwin@gmail.com>
2020-09-02 16:51:46 -0700

    images

---
+++ build/precache-manifest.06c182a2247d1300a149dbe4152cd723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1bc158f4e54fe1e4431c50e3ba3b715",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abf1f800508e5191afb8",
+    "url": "/static/css/main.9ea76d7c.chunk.css"
+  },
+  {
+    "revision": "fcb0842cc1dc2b85d0ff",
+    "url": "/static/js/2.5bbfff06.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.5bbfff06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf1f800508e5191afb8",
+    "url": "/static/js/main.a6506a63.chunk.js"
+  },
+  {
+    "revision": "97dd0eeb3304547d5553",
+    "url": "/static/js/runtime-main.05223113.js"
+  }
+]);
